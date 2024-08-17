8ce1bd1d09f3324e1248e30a5eb4bd60c763e159
elghandori1 <mohamedelghandori4@gmail.com>
2024-08-17 22:13:38 +0100

    fix same cases in parssing

--- check_map/get_map.c
+++ check_map/get_map.c
@@ -18,46 +18,59 @@ int	i;
 	return (len);
 }
 
-void surround_map_with_walls(t_cub3d* cub3d, char **map, int rows)
+void check_map_last(t_cub3d *cub3d,char **check_last)
 {
     int i = 0;
-    int j;
-    while (map[0][i] && map[0][i] != '\n')
+    while (check_last[i])
     {
-        if (map[0][i] != '1' && map[0][i] != ' ')
-            ft_error(cub3d, "Error: Map is not surrounded by walls.\n");
+        if (ft_strncmp(check_last[i], "WE", 2) == 0 || ft_strncmp(check_last[i], "NO", 2) == 0 \
+            || ft_strncmp(check_last[i], "EA", 2) == 0 || ft_strncmp(check_last[i], "SO", 2) == 0 \
+            || ft_strncmp(check_last[i], "C", 1) == 0 || ft_strncmp(check_last[i], "F", 1) == 0)
+            break;
+        if (ft_strncmp(check_last[i], "1", 1) == 0 || ft_strncmp(check_last[i], "0", 1) == 0)
+            ft_error(cub3d,"the map must be the last !\n");
         i++;
     }
-    i = 0;
-    while (map[rows - 1][i] && map[rows - 1][i] != '\n')
+}
+
+void validate_map_borders(t_cub3d *cub3d)
+{
+     int i,j;
+     int rows;
+     i=0;
+    while (cub3d->map->map[0][i] && cub3d->map->map[0][i] != '\n')
     {
-        if (map[rows - 1][i] != '1' && map[rows - 1][i] != ' ')
+        if (cub3d->map->map[0][i] != '1' && cub3d->map->map[0][i] != ' ')
+            ft_error(cub3d, "Error: Map is not surrounded by walls.\n");
+         i++;
+    }
+    i = 0;
+    rows = cub3d->map->len - 1;
+    while (cub3d->map->map[rows][i] && cub3d->map->map[rows][i] != '\n')
+    {
+        if (cub3d->map->map[rows][i] != '1' && cub3d->map->map[rows][i] != ' ')
             ft_error(cub3d, "Error: Map is not surrounded by walls.\n");
         i++;
     }
     i = 0;
     while (i < rows)
     {
-        if (map[i][0] != '1' && map[i][0] != ' ')
+        if (cub3d->map->map[i][0] != '1' && cub3d->map->map[i][0] != ' ')
             ft_error(cub3d, "Error: Map is not surrounded by walls.\n");
-        j = ft_strlen(map[i]) - 1;
-        while (j >= 0 && (map[i][j] == '\0' || map[i][j] == '\n' || map[i][j] == ' '))
-            j--;
-        if (j >= 0 && map[i][j] != '1' && map[i][j] != ' ')
+        j = ft_strlen(cub3d->map->map[i]) - 2;
+        if (j >= 0 && cub3d->map->map[i][j] != '1' && cub3d->map->map[i][j] != ' ')
             ft_error(cub3d, "Error: Map is not surrounded by walls.\n");
-
-        i++;
+        i++;  
     }
 }
-
 
 void get_map(t_cub3d *cub3d)
 {
     int i, j, len;
+    int map_start = 0;
 
     j = 0;
     i = 0;
-    cub3d->map->max_len = 0;
     cub3d->map->len = map_len(cub3d->map->content);
     len = cub3d->map->len;
     if (len == 0)
@@ -65,16 +78,22 @@ void get_map(t_cub3d *cub3d)
     cub3d->map->map = malloc((len + 1) * sizeof(char *));
     if (!cub3d->map->map)
         ft_error(cub3d, "Error: Memory allocation failed for map.\n");
-    while (cub3d->map->content[i] && !(cub3d->map->content[i][0] == '1'))
-        i++;
+    while (cub3d->map->content[i] && !map_start)
+    {
+        if (ft_search(cub3d->map->content[i][0], "01 "))
+            map_start = 1;
+        else
+            i++;
+    }
     while (cub3d->map->content[i])
     {
         cub3d->map->map[j] = ft_strdup(cub3d->map->content[i]);
-        if (ft_strlen(cub3d->map->map[j]) > cub3d->map->max_len)
-            cub3d->map->max_len = ft_strlen(cub3d->map->map[j]);
-        j++;
+        if (!cub3d->map->map[j])
+            ft_error(cub3d, "Error: Memory allocation failed while copying map.\n");
         i++;
+        j++;
     }
     cub3d->map->map[j] = NULL;
-    surround_map_with_walls(cub3d, cub3d->map->map, j);
+    check_map_last(cub3d,cub3d->map->content);
+    validate_map_borders(cub3d);
 }
