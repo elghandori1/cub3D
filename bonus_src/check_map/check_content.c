679cb8aad8c94c2df264851eac5a000f15a9fd6a
mg <mg@ubunto.myguest.virtualbox.org>
2024-09-28 22:11:56 +0200

    fix case space before

--- bonus_src/check_map/check_content.c
+++ bonus_src/check_map/check_content.c
@@ -8,8 +8,13 @@ void valid_content(t_game *cub3d)
  while (cub3d->data->content[++i])
  {
   if(ft_search(cub3d->data->content[i][0], " 01"))
-    break;
-  if (ft_search(cub3d->data->content[i][0], " 10FC\n"))
+  {
+    if(ft_search(cub3d->data->content[i][0], " ") && !ft_strchr(cub3d->data->content[i],'1'))
+      ft_error(cub3d, "Error: bad character in content of file\n");
+    else
+      break;
+  }
+  if (ft_search(cub3d->data->content[i][0], "10FC\n"))
    continue ;
   else if (ft_strncmp(cub3d->data->content[i], "WE", 2)
    && ft_strncmp(cub3d->data->content[i], "NO", 2)
@@ -17,6 +22,24 @@ void valid_content(t_game *cub3d)
    && ft_strncmp(cub3d->data->content[i], "SO", 2))
    ft_error(cub3d, "Error: bad character in content of file\n");
  }
+}
+
+void check_content(t_game *cub3d)
+{ 
+	if (!check_map_last(cub3d))
+    {
+    valid_content(cub3d);
+    check_identifier(cub3d);
+    check_textures_path(cub3d->data->content, cub3d);
+    get_textures(cub3d);
+    get_colors(cub3d);
+    get_map(cub3d);
+    get_square_map(cub3d);
+    check_player(cub3d);
+    if (!check_walls(cub3d))
+        ft_error(cub3d, "Error: Map borders are invalid!\n");
+    }else
+		ft_error(cub3d,"Error: The map must be the last!\n");
 }
 
 void check_content(t_game *cub3d)
